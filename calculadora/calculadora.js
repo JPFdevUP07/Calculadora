a22517b590ee32893d0702dc203412b5972210ce
JPFdevUP07 <joaopedrofh07@gmail.com>
2024-08-16 14:02:10 -0300

    Update calculadora.js

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,24 +1,26 @@
-function insert(num) 
-{
-    // Obtém o conteúdo atual do elemento HTML com o ID 'resultado'.
-    var numero = document.getElementById('resultado').innerHTML; 
-    
-    // Atualiza o conteúdo do elemento 'resultado', adicionando o número (ou símbolo) passado na função.
-    document.getElementById('resultado').innerHTML = numero + num; 
-}
-
+function insert(num)
+    {
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num;
+    }
 function clean()
-{
-    // Limpa o conteúdo do elemento HTML com o ID 'resultado', deixando-o vazio.
-    document.getElementById('resultado').innerHTML = "";
-}
-
+    {
+        document.getElementById('resultado').innerHTML = "";
+    }
 function back()
-{
-    // Obtém o conteúdo atual do elemento HTML com o ID 'resultado'.
-    var resultado = document.getElementById('resultado').innerHTML; 
-    
-    // Atualiza o conteúdo do elemento 'resultado' removendo o último caractere (útil para a funcionalidade de "backspace").
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
-
+    {
+        var resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    }
+function calcular()
+    {
+        var resultado = document.getElementById('resultado').innerHTML;
+        if(resultado)
+        {
+            document.getElementById('resultado').innerHTML = eval(resultado);
+        }
+        else
+        {
+            document.getElementById('resultado').innerHTML = "Nada..."
+        }
+    }
